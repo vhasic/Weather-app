cf671817bbd3fac2822dd3dfd804a65be8c9d49c
Vahidin <vhasic1@etf.unsa.ba>
2022-08-09 16:13:53 +0200

    Updated environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl: 'https://api.openweathermap.org/data/2.5/weather',
+  APIKey: 'e0eb5905c7dd3fe7b8c68f9723009c83'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   weatherApiBaseUrl: 'https://api.openweathermap.org/data/2.5/weather',
-  APIKey: 'e0eb5905c7dd3fe7b8c68f9723009c83',
+  APIKey: 'e0eb5905c7dd3fe7b8c68f9723009c83'
 };
 
 /*
